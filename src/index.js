aeab8f04c2bda4c23d8fb6f3158690d863539e3b
Joe Crick <joe@josephcrick.com>
2018-03-15 17:09:47 +0100

    Revert to use old simpl-r.

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import { render } from "react-dom";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import { I18nextProvider } from "react-i18next";
 import i18n from "../i18n/i18next";
+import rootStore from "src/stores/store";
+import { Provider } from "react-redux";
 import App from "./app";
 import Header from "components/shared/header/header";
 import Footer from "components/shared/footer/footer";
 import PageNotFound from "components/shared/error-pages/page-not-found";
 import ErrorBoundary from "components/shared/error-boundary/error-boundary";
 import countryCode from "./utils/country-code";
-import { Provider } from "react-redux";
-import rootStore from "src/stores/store";
 import "./site.less";
 import styles from "./site.less";
 
