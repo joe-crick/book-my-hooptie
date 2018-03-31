33b86e7bb763344927e837513e6e68bbfe1a50f7
Joe Crick <joe@josephcrick.com>
2018-03-31 08:41:57 +0200

    Add Redux DevTools.

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,6 +1,7 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import { rootReducer } from "reduxigen/actions";
 import DEFAULT from "src/state/state";
 
-export default createStore(rootReducer(DEFAULT), applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export default createStore(rootReducer(DEFAULT), composeEnhancers(applyMiddleware(thunk)));
